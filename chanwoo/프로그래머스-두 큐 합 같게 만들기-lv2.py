5a360cc677c6f3bdfb5839e432eea0ee88092175
CHANWOO BAE <39613774+chanwooDev@users.noreply.github.com>
2022-10-29 00:11:58 +0900

    Modify 프로그래머스-두 큐 합 같게 만들기-lv2.py

--- chanwoo/프로그래머스-두 큐 합 같게 만들기-lv2.py
+++ chanwoo/프로그래머스-두 큐 합 같게 만들기-lv2.py
@@ -2,20 +2,29 @@ from collections import deque
 
 def solution(queue1, queue2):
     q1 = deque(queue1)
+    q1s = sum(queue1)
     q2 = deque(queue2)
-    max_count = len(queue1)+len(queue2)
+    q2s = sum(queue2)
+
+    max_count = (len(queue1)+len(queue2))*2
     count = 0
     while count <= max_count:
-        if sum(q1) < sum(q2):
-            q1.append(q2.popleft)
-        elif sum(q1) > sum(q2):
-            q2.append(q1.popleft)
+        if q1s < q2s:
+            k = q2.popleft()
+            q1.append(k)
+            q1s += k
+            q2s -= k
+        elif q1s > q2s:
+            k = q1.popleft()
+            q2.append(k)
+            q2s += k
+            q1s -= k
         else:
             break
         count += 1
-    if count == max_count:
+    if count > max_count:
         return -1
     else:
         return count
 
-print(solution([3, 2, 7, 2],[4, 6, 5, 1]))
+print(solution( [1, 1, 1, 1, 1], [1, 1, 1, 9, 1] ))
