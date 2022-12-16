7f142e79ad76b9e63b12398f36c23aaea2d67b5b
dionidip <paulus0617@gmail.com>
2022-12-16 23:54:12 +0900

    2022-12-16

--- changyeop/백준_11000_G5.java
+++ changyeop/백준_11000_G5.java
@@ -3,12 +3,11 @@ package changyeop;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
-class Main {
+class Main11000 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
