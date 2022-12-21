99996373ab2497a3eebad76e55aedcb6685956d5
dionidip <paulus0617@gmail.com>
2022-12-21 18:55:11 +0900

    2022-12-21

--- changyeop/백준_1092_G5.java
+++ changyeop/백준_1092_G5.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
-class Main {
+class Main1092 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
